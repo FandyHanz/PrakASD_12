2c36b169189f206c8466acbc1a7f205c28c2fc7b
FandyHanz <hanzyura28@gmail.com>
2024-02-20 08:30:10 +0700

    P2_Percobaan2_12

---
+++ PrakASD_12/src/P2/BukuMain12.java
@@ -0,0 +1,17 @@
+package P2;
+
+public class BukuMain12 {
+    public static void main(String[] args) {
+        Buku12 bk1 = new Buku12();
+        bk1.judul = "Today Ends Tomorrow Come";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 710000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
