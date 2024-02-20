232e84e2c9ff430be7d415e45b536bf63c1ce4a1
FandyHanz <hanzyura28@gmail.com>
2024-02-20 08:51:30 +0700

    P2_Percobaan3_12

--- PrakASD_12/src/P2/Buku12.java
+++ PrakASD_12/src/P2/Buku12.java
@@ -4,12 +4,24 @@ public class Buku12 {
     String judul, pengarang;
     int halaman, stok, harga;
 
+    public Buku12(){
+
+    }
+
+    public Buku12(String jud, String pg, int hal, int stok, int har){
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
         System.out.println("Jumlah halaman: " + halaman);
         System.out.println("Sisa stok: " + stok);
-        System.out.println("Harga: Rp " + judul);
+        System.out.println("Harga: Rp " + harga);
     }
 
     void terjual(int jml) {
@@ -26,3 +38,4 @@ public class Buku12 {
         harga = hrg;
     }
 }
+
