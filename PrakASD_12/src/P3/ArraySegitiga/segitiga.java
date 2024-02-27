9b5480b111902b34f08896edbd874f427af50b88
FandyHanz <hanzyura28@gmail.com>
2024-02-27 17:32:22 +0700

    P3_Percobaan3_Pertanyaan2_12

---
+++ PrakASD_12/src/P3/ArraySegitiga/segitiga.java
@@ -0,0 +1,11 @@
+package P3.ArraySegitiga;
+
+public class segitiga {
+    public int alas;
+    public int tinggi;
+
+    public segitiga (int a, int t){
+        alas = a;
+        tinggi = t;
+    }
+}
