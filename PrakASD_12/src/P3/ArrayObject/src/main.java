31bbbdffd330a8616edaf193443b01554becba9e
FandyHanz <hanzyura28@gmail.com>
2024-02-27 16:21:08 +0700

    P3_Percobaan2_Pertanyaan4_12

--- PrakASD_12/src/P3/ArrayObject/src/main.java
+++ PrakASD_12/src/P3/ArrayObject/src/main.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-
+        int j = 0;
         Scanner input12 = new Scanner(System.in);
-        PersegiPanjang[] ppArray = new PersegiPanjang[3];
 
-        for (int i = 0; i < 3; i++) {
+        System.out.print("Masukan kolom yang diinginkan: ");
+        j = input12.nextInt();
+        PersegiPanjang[] ppArray = new PersegiPanjang[j];
+
+        for (int i = 0; i < j; i++) {
 
             ppArray[i] = new PersegiPanjang();
             System.out.println("persegi panjang ke- " + i );
@@ -17,8 +20,8 @@ public class main {
             System.out.print("Maukan lebar: ");
             ppArray[i].lebar = input12.nextInt();
         }
-        
-        for (int i = 0; i < 3; i++) {
+
+        for (int i = 0; i < j; i++) {
             System.out.println("Persegi panjang ke-" + i);
             System.out.println("Panjang: " + ppArray[i].panjang + ", Lebar: " + ppArray[i].lebar);
         }
