b15d005120cbdb43d28f9aa784877224dc900705
FandyHanz <hanzyura28@gmail.com>
2024-02-27 13:20:41 +0700

    P3_Percobaan1_12

---
+++ PrakASD_12/src/P3/ArrayObject/src/main.java
@@ -0,0 +1,23 @@
+package P3.ArrayObject.src;
+
+public class main {
+    public static void main(String[] args) {
+        PersegiPanjang [] ppArray = new PersegiPanjang[3];
+
+        ppArray[0] = new PersegiPanjang();
+        ppArray[0].panjang = 110;
+        ppArray[0].lebar = 30;
+
+        ppArray[1] = new PersegiPanjang();
+        ppArray[1].panjang = 80;
+        ppArray[1].lebar = 40;
+
+        ppArray[2] = new PersegiPanjang();
+        ppArray[2].panjang = 100;
+        ppArray[2].lebar = 20;
+
+        System.out.println("Persegi panjang ke-0, Panjang: " + ppArray[0].panjang + ", Lebar: " + ppArray[0].lebar);
+        System.out.println("Persegi panjang ke-1, Panjang: " + ppArray[1].panjang + ", Lebar: " + ppArray[1].lebar);
+        System.out.println("Persegi panjang ke-2, Panjang: " + ppArray[2].panjang + ", Lebar: " + ppArray[2].lebar);
+    }
+}
