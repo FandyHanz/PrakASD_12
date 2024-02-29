e2e11db37549d6e38142779783b620140e4da005
FandyHanz <hanzyura28@gmail.com>
2024-02-29 12:18:38 +0700

    P3_Tugas3_12

--- PrakASD_12/src/P3/ArrayObject/src/tugas2/main.java
+++ PrakASD_12/src/P3/ArrayObject/src/tugas2/main.java
@@ -2,6 +2,9 @@ package P3.ArrayObject.src.tugas2;
 import java.util.Scanner;
 
 public class main {
+    public static double [] data = new double [3];
+    public static double sum = 0;
+    public static double max = 0;
     public static void main(String[] args) {
         Scanner Strinput12 = new Scanner (System.in);
         Scanner Intinput12 = new Scanner (System.in);
@@ -28,6 +31,27 @@ public class main {
             System.out.print("Ipk: " + pdm[i].ipk + "\n");
             System.out.println();
         }
+        for (int i = 0; i < pdm.length; i++) {
+            data[i] = pdm[i].ipk;
+        }
+        rerata(data);
+        pencarirata(data);
+    }
+    public static void rerata (double nilai[]){
+        for (int i = 0; i < nilai.length; i++) {
+            sum += nilai[i];
+        }
+        System.out.println("total rata rata ipk para mahasiswa sekitar: " + sum / nilai.length);
+    }
 
+    public static void pencarirata (double nilai[]){
+        int column= 0;
+        for (int i = 0; i < nilai.length; i++) {
+            if (nilai[i] > max){
+                column = i;
+                max = nilai[column];
+            }
+        }
+        System.out.println("pemegang dengan ipk sebesar " + max );
     }
 }
