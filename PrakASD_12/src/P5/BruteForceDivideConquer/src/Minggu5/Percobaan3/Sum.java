fde8829f424a7e2b905f2fdabf078fda7d83dfe6
FandyHanz <hanzyura28@gmail.com>
2024-03-22 09:40:48 +0700

    P5_Percobaan3_Pertanyaan3_12

--- PrakASD_12/src/P5/BruteForceDivideConquer/src/Minggu5/Percobaan3/Sum.java
+++ PrakASD_12/src/P5/BruteForceDivideConquer/src/Minggu5/Percobaan3/Sum.java
@@ -1,30 +1,30 @@
 package P5.BruteForceDivideConquer.src.Minggu5.Percobaan3;
 
 public class Sum {
-    public static int elemen;
-    public static double keuntungan[], total;
+    public int elemen;
+    public double keuntungan[], total;
 
     public Sum (int elemen){
         this.elemen = elemen;
         this.keuntungan = new double[elemen];
         this.total = total;
     }
-    public static double TotalBF(double arr[]){
+    public double TotalBF(double arr[]){
         for (int i = 0; i < elemen; i++) {
             total = total + arr[i];
         }
         return total;
     }
 
-    public static double TotalDC(double arr[], int l, int r){
+    public double TotalDC(double arr[], int l, int r){
         if (l == r){
             return arr[l];
         }
         else if (l < r){
-            int mid = (l / r) + 2;
-            double lsum = TotalDC(arr, l, mid - r);
-            double rsum = TotalDC(arr, mid + l, r);
-            return lsum + rsum + arr[mid];
+            int mid = (l + r) / 2;
+            double lsum = TotalDC(arr, l, mid);
+            double rsum = TotalDC(arr, mid + 1, r);
+            return lsum + rsum;
         }
         return 0;
     }
--- PrakASD_12/src/P5/BruteForceDivideConquer/src/Minggu5/Percobaan3/mainSum.java
+++ PrakASD_12/src/P5/BruteForceDivideConquer/src/Minggu5/Percobaan3/mainSum.java
@@ -1,24 +1,31 @@
 package P5.BruteForceDivideConquer.src.Minggu5.Percobaan3;
+
 import java.util.Scanner;
+
 public class mainSum {
     public static void main(String[] args) {
         Scanner input12 = new Scanner(System.in);
 
-        System.out.println("=============================");
-        System.out.println("Program Menghitung Keuntungan Total (Satuan juta misal 5.9):");
-        System.out.print("Masukan jumlah bulan: ");
-        int elm = input12.nextInt();
+        System.out.print("Silahkan masukan jumlah perusahaan yang ingin di data: ");
+        int counter = input12.nextInt();
+        for (int i = 0; i < counter; i++) {
+            System.out.println("Masukan data untuk perusahaan ke -" + (i+1) + ": ");
+            System.out.println("=============================");
+            System.out.println("Program Menghitung Keuntungan Total (Satuan juta misal 5.9):");
+            System.out.print("Masukan jumlah bulan: ");
+            int elm = input12.nextInt();
 
-        Sum sm = new Sum(elm);
-        System.out.println("================================");
-        for (int i = 0; i < sm.elemen; i++) {
-            System.out.print("Masukan untung bulan ke -" + (i+1) + ": ");
-            sm.keuntungan[i] = input12.nextDouble();
+            Sum sm = new Sum(elm);
+            System.out.println("================================");
+            for (int j = 0; j < sm.elemen; j++) {
+                System.out.print("Masukan untung bulan ke -" + (j + 1) + ": ");
+                sm.keuntungan[j] = input12.nextDouble();
+            }
+
+            System.out.println("Agloritma bruteforce");
+            System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah: " + sm.TotalBF(sm.keuntungan));
+            System.out.println("Agloritma divide and conqueror");
+            System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah: " + sm.TotalDC(sm.keuntungan, 0, sm.elemen - 1));
         }
-
-        System.out.println("Agloritma bruteforce");
-        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah: " + sm.TotalBF(sm.keuntungan));
-        System.out.println("Agloritma divide and conqueror");
-        System.out.println("Total keuntungan perusahaan selama " + sm.elemen + " bulan adalah: " + sm.TotalDC(sm.keuntungan, 0, sm.elemen -1));
     }
 }
